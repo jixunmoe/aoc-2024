2b1c2a1a4d6dc19dea42e9c75f3919555132e01f
Jixun <8041017-jixunmoe@users.noreply.gitlab.com>
2024-12-01 22:24:11 +0000

    init: repo layout

---
+++ generate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+toc=""
+for i in $(seq 25); do
+  day="day-$(printf "%02d" $i)"
+
+  prev="[< prev]"
+  next="[next >]"
+
+  if [[ $i -ne 1 ]]; then
+    prev="[[< prev]](../day-$(printf "%02d" $((i-1)))/README.MD)"
+  fi
+  if [[ $i -ne 25 ]]; then
+    next="[[next >]](../day-$(printf "%02d" $((i+1)))/README.MD)"
+  fi
+
+  if [[ ! -f "aoc-2024/$day/README.MD" ]]; then
+    mkdir -p "aoc-2024/$day"
+    cat > "aoc-2024/$day/README.MD" <<EOF
+# Day $i: TBD
+
+[[^ up]](../../README.MD) $prev $next <!-- [[solution ✨]](./solve.py) -->
+
+<!-- article begin -->
+
+<!-- article end -->
+
+---
+
+Source: https://adventofcode.com/2024/day/$i
+
+EOF
+  fi
+
+  toc="$toc- [Day $i](./aoc-2024/$day/README.MD) 🚧\n"
+done
+
+echo -e $toc >> README.MD
