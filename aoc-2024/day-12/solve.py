624070ec2f4f75b7b87a1657604d7c07e6e481b0
Jixun Moe <5713045+jixunmoe@users.noreply.github.com>
2024-12-12 22:01:15 +0000

    refactor: speedup python solution with numpy

--- aoc-2024/day-12/solve.py
+++ aoc-2024/day-12/solve.py
@@ -1,7 +1,24 @@
 import argparse
-from collections import Counter
 from itertools import chain, combinations
 import re
+from typing import Tuple, Any
+
+import numpy as np
+from numpy import ndarray, dtype, float64
+
+
+def count_lines(data: ndarray[tuple[int, int], np.uint8]):
+    found = 0
+    for row in data:
+        should_count = True
+        for item in row:
+            if should_count and item == 1:
+                should_count = False
+                found += 1
+            elif item != 1:
+                should_count = True
+    return found
+
 
 
 class Garden:
@@ -22,20 +39,20 @@ class Garden:
 
 
 class GardenExplorer:
+    visited: ndarray[tuple[int, int], np.uint8]
     garden: Garden
 
     def __init__(self, garden: Garden):
         self.garden = garden
 
         w, h = garden.size
-
-        self.visited = [[False] * w for _ in range(h)]
+        self.visited = np.zeros(shape=(w, h), dtype=np.uint8)
 
     def find_regions(self):
         w, h = self.garden.size
         for y in range(h):
             for x in range(w):
-                if self.visited[y][x]:
+                if self.visited[y,x]:
                     continue
                 yield self.explore_region((x, y))
 
@@ -44,20 +61,24 @@ class GardenExplorer:
         w, h = self.garden.size
         value = self.garden.grid[y][x]
 
-        self.visited[y][x] = True
+        self.visited[y,x] = 1
         pos_to_explore = {pos}
 
         area = 1
         perimeter = 4
 
-        all_visited = {pos}
-        vertical_lines = [[0] * (w + 1) for _ in range(h + 1)]
-        horizontal_lines = [[0] * (w + 1) for _ in range(h + 1)]
+        visited_in_session = {pos}
+        scan_line_shape=(w + 1, h + 1)
+        vertical_lines: ndarray[tuple[int, int], np.uint8] = np.zeros(shape=scan_line_shape, dtype=np.uint8)
+        horizontal_lines: ndarray[tuple[int, int], np.uint8] = np.zeros(shape=scan_line_shape, dtype=np.uint8)
 
-        vertical_lines[x][y] = 1
-        vertical_lines[x + 1][y] = 1
-        horizontal_lines[y][x] = 1
-        horizontal_lines[y + 1][x] = 1
+        min_x, max_x = x, x
+        min_y, max_y = y, y
+
+        vertical_lines[x,y] = 1
+        vertical_lines[x+1,y] = 1
+        horizontal_lines[x,y] = 1
+        horizontal_lines[x,y+1] = 1
 
         while len(pos_to_explore) > 0:
             next_pos_set = set()
@@ -68,47 +89,43 @@ class GardenExplorer:
                     if next_value != value:
                         continue
                     x, y = next_pos
-                    if self.visited[y][x]:
+                    if self.visited[y,x]:
                         perimeter -= 1
                         continue
                     # not visited
-                    self.visited[y][x] = True
+                    self.visited[y,x] = True
                     next_pos_set.add(next_pos)
-                    all_visited.add(next_pos)
+                    visited_in_session.add(next_pos)
                     perimeter += 3
                     area += 1
 
-                    vertical_lines[x][y] += 1
-                    vertical_lines[x + 1][y] += 1
-
-                    horizontal_lines[y][x] += 1
-                    horizontal_lines[y + 1][x] += 1
+                    vertical_lines[x,y] += 1
+                    vertical_lines[x+1,y] += 1
+                    horizontal_lines[x,y] += 1
+                    horizontal_lines[x,y+1] += 1
+                    min_x, min_y = min(min_x, x), min(min_y, y)
+                    max_x, max_y = max(max_x, x), max(max_y, y)
             pos_to_explore = next_pos_set
 
-        # Transpose vertical lines
-        # vertical_lines_t = [[vertical_lines[x][y] for x in range(w + 1)] for y in range(w + 1)]
-        vertical_lines_t = vertical_lines
-        temp = '\n'.join([''.join(map(str, row)) for row in chain.from_iterable([vertical_lines_t, horizontal_lines])])
-        sides = len(list(re.findall(r'1+', temp)))  # greedy match
+        max_x += 2
+        max_y += 2
 
-        for ((x1, y1), (x2, y2)) in combinations(all_visited, 2):
+        nh = horizontal_lines[min_x:max_x, min_y:max_y].transpose()
+        nv = vertical_lines[min_x:max_x, min_y:max_y]
+        sides = count_lines(nh) + count_lines(nv)
+
+        for ((x1, y1), (x2, y2)) in combinations(visited_in_session, 2):
             dx = x2 - x1
             dy = y2 - y1
 
+            # Check if the distance between two points is sqrt(2)
             if dx * dx + dy * dy == 2:
                 p1 = (x1 + dx, y1)
                 p2 = (x1, y1 + dy)
 
-                if p1 not in all_visited and p2 not in all_visited:
+                # They break!
+                if p1 not in visited_in_session and p2 not in visited_in_session:
                     sides += 2
-
-        # for y in range(h - 1):
-        #     for x in range(w - 1):
-        #         a, b, c, d = map(lambda p: p in all_visited, ((y, x), (y, x + 1), (y + 1, x), (y + 1, x + 1)))
-        #         if (a, b, c, d) in ((False, True, True, False), (True, False, False, True)):
-        #             print(f"Checking: {(x, y)}: {a} {b} {c} {d}")
-        #             sides += 2
-        #             continue
 
         return value, area, perimeter, sides
 
